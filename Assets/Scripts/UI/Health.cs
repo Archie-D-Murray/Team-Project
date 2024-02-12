2dc5b03968198e4871f15f4fbe01496353163180
Archie-D-Murray <23684612@stu.mmu.ac.uk>
2024-02-12 04:46:38 +0000

    test: Moved dummy classes to own file to start testing

---
+++ Assets/Scripts/UI/Health.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+using UnityEngine;
+
+namespace UI {
+    [RequireComponent(typeof(Stats))]
+    public class Health : MonoBehaviour {
+        public float getPercentHealth => Mathf.Clamp01(currentHealth / maxHealth);
+        public float getCurrentHealth => currentHealth;
+        public float getMaxHealth => maxHealth;
+
+        public Action<float> onDamage;
+        public Action onDeath;
+
+        private float currentHealth;
+        private float maxHealth;
+
+        private void Awake() {
+            Stats stats = GetComponent<Stats>();
+            stats.updateStat += UpdateMaxHealth;
+        }
+
+        private void UpdateMaxHealth(StatType type, float health) {
+            if (type != StatType.Health) {
+                return;
+            }
+            float diff = health - currentHealth;
+            maxHealth = health;
+            Damage(diff);
+        }
+
+        public void Damage(float damage) {
+            if (currentHealth == 0.0f) { //Don't damage dead things!
+                return;
+            }
+            damage = Mathf.Max(damage, 0.0f);
+            if (damage != 0.0f) {
+                currentHealth = Mathf.Max(0.0f, currentHealth - damage);
+                onDamage?.Invoke(damage);
+            }
+            if (currentHealth == 0.0f) {
+                Debug.Log($"{name} is dead");
+                onDeath?.Invoke();
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/UI/Stats.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+
+using UnityEngine;
+
+namespace UI {
+    public class Stats : MonoBehaviour {
+
+        #region Enum => String cache
+        private const string Health = "Health";
+        private const string AttackSpeed = "Attack Speed";
+        private const string Speed = "Speed";
+        private const string Damage = "Damage";
+        private const string Magic = "Magic";
+        #endregion
+
+        public Dictionary<StatType, float> statDict => statDict;
+        public Action<StatType, float> updateStat;
+
+        public string GetStatDisplay(StatType type) {
+            return $"{GetStatName(type)}: {statDict[type]}";
+        }
+
+        public string GetStatName(StatType type) {
+            return type switch {
+                StatType.Health => Health,
+                StatType.AttackSpeed => AttackSpeed,
+                StatType.Speed => Speed,
+                StatType.Damage => Damage,
+                StatType.Magic => Magic,
+                _ => string.Empty
+            };
+        }
+
+        public bool GetStat(StatType type, out float stat) { 
+            return statDict.TryGetValue(type, out stat);
+        }
+
+        public void UpdateStat(StatType type, float amount, bool setToValue = false) {
+            if (!statDict.ContainsKey(type)) {
+                Debug.LogError($"Stat type {type} was not an entry in stats!");
+                return;
+            }
+            statDict[type] = setToValue ? amount : statDict[type] + amount;
+            updateStat?.Invoke(type, statDict[type]);
+        }
+    }
+}
