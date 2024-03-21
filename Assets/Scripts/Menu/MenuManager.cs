e5635079f9191797168c2ba844b60579cedb4c6f
AWilton0104 <146730082+AWilton0104@users.noreply.github.com>
2024-03-21 11:55:48 +0000

    Started Base of main menu

---
+++ Assets/Scripts/Menu/MenuManager.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuManager : MonoBehaviour
+{
+    [SerializeField] private GameObject[] mainPage;
+    [SerializeField] private GameObject[] classPage;
+
+    private void Start() {
+        GoMainPage();    
+    }
+
+    public void GoClassPage() {
+        foreach(GameObject button in mainPage) {
+            button.SetActive(false);
+        }
+        foreach(GameObject button in classPage) {
+            button.SetActive(true);
+        }
+    }
+
+    public void GoMainPage() {
+        foreach (GameObject button in mainPage) {
+            button.SetActive(true);
+        }
+        foreach (GameObject button in classPage) {
+            button.SetActive(false);
+        }
+    }
+
+    public void StartGame(int playerClass) {
+        //singleton stuff idk lol
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void Quit() {
+        Application.Quit();
+    }
+}
